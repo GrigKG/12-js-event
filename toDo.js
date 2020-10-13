d84ab9304bae3b4e49929aba7482ea9333c1f228
GrigKG <38141067+GrigKG@users.noreply.github.com>
2020-10-13 09:38:19 +0300

    hw

--- toDo.js
+++ toDo.js
@@ -2,25 +2,23 @@ const root = document.getElementById('root');
 root.append(document.createTextNode("Add new Item"));
 const input = document.createElement('input');
 const buttonAdd = document.createElement('button');
-root.append(input, buttonAdd);
+const ol = document.createElement('ol');
+root.append(input, buttonAdd,ol);
 buttonAdd.append(document.createTextNode('new Item'));
 input.setAttribute('type', 'text');
 buttonAdd.addEventListener('click', addItem);
-let i = 0;
 
 function addItem(event) {
     const text = this.previousElementSibling.value;
-    const p = document.createElement('p');
-    const content = document.createTextNode(++i+". " + text);
+    const li = document.createElement('li');
+    const content = document.createTextNode(text);
     const buttonX = document.createElement('button');
     buttonX.append(document.createTextNode('X'));
     buttonX.addEventListener('click', delItem);
-    p.append(content,buttonX);
-    event.target.parentElement.append(p);
+    li.append(content,buttonX);
+    ol.append(li);
     event.target.previousElementSibling.value = '';
 }
 function delItem(event){
     event.target.parentElement.remove();
-    i--;
-
 }
